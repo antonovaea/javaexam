05324e34047c945d9cb6a67a6cec2d810c6d5465
Elizaveta Antonova <lizaantonova1@yandex.ru>
2021-08-11 13:23:51 +0300

    first test done

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,5 +1,8 @@
 package com.tsystems.javaschool.tasks.pyramid;
-
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class PyramidBuilder {
@@ -13,8 +16,55 @@ public class PyramidBuilder {
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+        List<Integer> input= new ArrayList<>(inputNumbers);
+        Collections.sort(input);
+        int[][] result = new int[0][0];
+        if (input.size() == 3){
+            result = threePyramid(input);
+        }
+
+
+        return result;
+    }
+
+    public int[][] threePyramid (List<Integer> l){
+        for (int i = 0; i <= 4; i = i + 2){
+            l.add(i, 0);
+        }
+        List<Integer> firstRow = l.subList(0, 3);
+        List<Integer> secondRow = l.subList(3, 6);
+        int [][] pyramid = new int[2][3];
+        for (int i = 0; i < 1; i++){
+            for (int j = 0; j < 3; j++){
+                pyramid[i][j] = firstRow.get(j);
+            }
+        }
+        for (int i = 1; i < 2; i++){
+            for (int j = 0; j < 3; j++){
+                pyramid[i][j] = secondRow.get(j);
+            }
+        }
+        return pyramid;
+    }
+
+    public int[][] sixPyramid (List<Integer> l){
+        for (int i = 0; i <= 4; i = i + 2){
+            l.add(i, 0);
+        }
+        List<Integer> firstRow = l.subList(0, 3);
+        List<Integer> secondRow = l.subList(3, 6);
+        int [][] pyramid = new int[2][3];
+        for (int i = 0; i < 1; i++){
+            for (int j = 0; j < 3; j++){
+                pyramid[i][j] = firstRow.get(j);
+            }
+        }
+        for (int i = 1; i < 2; i++){
+            for (int j = 0; j < 3; j++){
+                pyramid[i][j] = secondRow.get(j);
+            }
+        }
+        return pyramid;
     }
 
 
