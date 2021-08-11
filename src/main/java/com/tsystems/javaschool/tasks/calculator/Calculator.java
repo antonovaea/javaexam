72a006fadd815bdb466cfbb9821c1d36cd5fc697
Elizaveta Antonova <lizaantonova1@yandex.ru>
2021-08-11 09:08:27 +0300

    all tests done

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -14,82 +14,36 @@ public class Calculator {
      */
 
         public String evaluate(String input) {
-            String lastResult = null;
-            int countDots = 0;
-            Expression e = new Expression(input);
-            double v = e.calculate();
-            System.out.println(v);
-            String result = "";
-            double nan = Double.NaN;
-            //если в инпут присутствует точка, то дабл, если нет, то инт
-            ArrayList<Character> chars = new ArrayList<>();
-            int len = input.length();
-            for (int i = 0; i < len; i++){
-                chars.add(input.charAt(i));
-            }
-            for (int i = 0; i < chars.size(); i++){
-                if (chars.get(i) == '.'){
-                    countDots++;
+            try {
+                String lastResult = ""; //конечный результат
+                int countDots = 0; //счетчик наличия точки в уравнении, если есть, значит итоговое число double
+                Expression e = new Expression(input); //уравнение
+                double v = e.calculate(); //считаем
+                double nan = Double.NaN;
+                String nanStr = String.valueOf(nan);
+                String vNan = String.valueOf(v);
+                ArrayList<Character> chars = new ArrayList<>(); //создаем ArrayList со всеми символами уравнения
+                for (int i = 0; i < input.length(); i++){
+                    chars.add(input.charAt(i));
                 }
+                for (int i = 0; i < chars.size(); i++){ //проверяем наличие плавающей точки
+                    if (chars.get(i) == '.'){
+                        countDots++;
+                    }
+                }
+                if (countDots > 0 && !vNan.equals(nanStr)){ // && если стринговое значение v не "NaN"
+                    lastResult = String.valueOf(v); //если плавающая точка есть - кладем в итог double
+                } else if (countDots == 0 && !vNan.equals(nanStr)) { // && если стринговое значение v не "NaN"
+                    int intResult = (int) v;
+                    String newResult = String.valueOf(intResult); //иначе, кладем в итог int
+                    lastResult = newResult;
+                } else if (vNan.equals(nanStr)) { // && если стринговое значение v = "NaN" значит результат null
+                    lastResult = null;
+                }
+                return  lastResult;
+            } catch (NullPointerException e){
+                return null;
             }
-            if (countDots > 0){
-                lastResult = String.valueOf(v);
-            } else {
-                int intResult = (int) v;
-                String newResult = String.valueOf(intResult);
-                lastResult = newResult;
-            }
-            return  lastResult;
-            
-        } 
 
-
-    private static boolean isDigit(String s) throws NumberFormatException {
-        try {
-            Integer.parseInt(s);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
         }
-    }
-//    public static String evaluat(String input) {
-//
-//        int len = input.length();
-//        ArrayList operands = new ArrayList();
-//        ArrayList operators = new ArrayList();
-//
-//        for (int i = 0; i < len; i++) {
-//            if ((i + 1 <= len - 1)) {
-//                if (isDigit(input.charAt(i)) && isDigit(input.charAt(i + 1))) {
-//                    String temp = input.charAt(i) + "" + input.charAt(i + 1);
-//                    int toInt = Integer.parseInt(temp);
-//                    operands.add(toInt);
-//                    i = i+1;
-//                } else if (isDigit(input.charAt(i))) {
-//                    operands.add(input.charAt(i));
-//                } else {
-//                    operators.add(input.charAt(i));
-//                }
-//
-//            }
-//        }
-////
-////        int result = 0;
-////        result = result + (int) operands.get(0);
-////        for (int t = 0; t < operators.size(); t++) {
-////            char operator = (char) operators.get(t);
-////            if (operator == '+') {
-////                result = result + (int) operands.get(t + 1);
-////
-////            } else if (operator == '-') {
-////                result = result - (int) operands.get(t + 1);
-////            }
-////        }
-////        String stringResult = String.valueOf(result);
-////        return stringResult;
-//
-//
-//
-//    }
-
 }
